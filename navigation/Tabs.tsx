0ef6b87db3f4e286baef0811bf9e34da27d7e55a
Gabriel Lebrun <lebrungabriel75@gmail.com>
2023-03-24 00:38:39 +0100

    tabBar design first commit

--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -1,7 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React, { useEffect, useRef } from 'react';
-import { StyleSheet, TouchableOpacity, View, PixelRatio } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text, PixelRatio } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
+
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import MapSearchScreen from '../screens/MapSearchScreen/MapSearchScreen';
 import ChatScreen from '../screens/ChatScreen/ChatScreen';
@@ -23,88 +24,112 @@ const TabArr = [
   { route: 'Add', label: 'Vendre', icon: 'plus', component: AddScreen },
   { route: 'Chat', label: 'Chat', icon: 'comment', component: ChatScreen },
   { route: 'Profile', label: 'Profil', icon: 'user-alt', component: ProfileScreen },
-  // {
-  //   route: 'PlantProfile',
-  //   label: 'Détails',
-  //   icon: 'user-alt',
-  //   component: PlantProfileScreen,
-  // },
 ];
 
 const Tab = createBottomTabNavigator();
-
-const animate1 = {
-  0: { scale: 0.5, translateY: 7 },
-  0.92: { translateY: -14 },
-  1: { scale: 1.2, translateY: -14 },
-};
-const animate2 = { 0: { scale: 1.2, translateY: -24 }, 1: { scale: 1, translateY: 7 } };
-
-const circle1 = {
-  0: { scale: 0 },
-  0.3: { scale: 0.9 },
-  0.5: { scale: 0.2 },
-  0.8: { scale: 0.7 },
-  1: { scale: 1 },
-};
-const circle2 = { 0: { scale: 1 }, 1: { scale: 0 } };
-
-const TabButton: any = (props: any) => {
-  const { item, onPress, accessibilityState } = props;
-  const focused = accessibilityState.selected;
-  const viewRef: any = useRef(null);
-  const circleRef: any = useRef(null);
-  const textRef: any = useRef(null);
-
-  useEffect(() => {
-    if (focused) {
-      viewRef.current.animate(animate1);
-      circleRef.current.animate(circle1);
-      textRef.current.transitionTo({ scale: 1 });
-    } else {
-      viewRef.current.animate(animate2);
-      circleRef.current.animate(circle2);
-      textRef.current.transitionTo({ scale: 0 });
-    }
-  }, [focused]);
-
-  return (
-    <TouchableOpacity onPress={onPress} activeOpacity={1} style={styles.container}>
-      <Animatable.View ref={viewRef} duration={325} style={styles.navbarContainer}>
-        <View style={[styles.btn, { borderWidth: focused ? 2 : 0, borderColor: '#ffffff' }]}>
-          <Animatable.View ref={circleRef} style={styles.circle} />
-          <FontAwesome5 name={item.icon} size={21} color={focused ? '#3FA96A' : '#ffffff'} />
-        </View>
-        <Animatable.Text ref={textRef} style={styles.text}>
-          {item.label}
-        </Animatable.Text>
-      </Animatable.View>
-    </TouchableOpacity>
-  );
-};
 
 const BottomTabs = () => (
   <Tab.Navigator
     screenOptions={{
       headerShown: false,
       tabBarStyle: {
-        height: 75,
+        height: 60,
+        position: 'relative',
       },
     }}
   >
-    {TabArr.map((item, index) => {
-      return (
-        <Tab.Screen
-          key={index}
-          name={item.route}
-          component={item.component}
-          options={{
-            tabBarShowLabel: false,
-            tabBarButton: (props) => <TabButton {...props} item={item} />,
-          }}
-        />
-      );
-    })}
+    <Tab.Screen
+      name='Home'
+      component={HomeScreen}
+      options={{
+        tabBarLabel: () => null,
+        tabBarIcon: ({ focused }) => (
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <FontAwesome5 name='home' size={21} color={focused ? '#344E41' : '#9DB2CE'} />
+            <Text style={{ color: focused ? '#344E41' : '#9DB2CE', fontSize: 12 }}>Accueil</Text>
+          </View>
+        ),
+      }}
+    />
+
+    <Tab.Screen
+      name='Search'
+      component={MapSearchScreen}
+      options={{
+        tabBarLabel: () => null,
+        tabBarIcon: ({ focused }) => (
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <FontAwesome5 name='search' size={21} color={focused ? '#344E41' : '#9DB2CE'} />
+            <Text style={{ color: focused ? '#344E41' : '#9DB2CE', fontSize: 12 }}>Rechercher</Text>
+          </View>
+        ),
+      }}
+    />
+
+    <Tab.Screen
+      name='Add'
+      component={AddScreen}
+      options={{
+        tabBarLabel: () => null,
+        tabBarIcon: ({ focused }) => (
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#C3E2D3',
+              width: 70,
+              height: 70,
+              borderRadius: '100%',
+              position: 'absolute',
+              bottom: 10,
+            }}
+          >
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: '#344E41',
+                width: focused ? '100%' : '90%',
+                height: focused ? '100%' : '90%',
+                borderRadius: '100%',
+              }}
+            >
+              <FontAwesome5 name='plus' size={21} color='white' />
+              <Text style={{ color: 'white', fontSize: 10 }}>Vendre</Text>
+            </View>
+          </View>
+        ),
+      }}
+    />
+
+    <Tab.Screen
+      name='Chat'
+      component={ChatScreen}
+      options={{
+        tabBarLabel: () => null,
+        tabBarIcon: ({ focused }) => (
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <FontAwesome5 name='comment' size={21} color={focused ? '#344E41' : '#9DB2CE'} />
+            <Text style={{ color: focused ? '#344E41' : '#9DB2CE', fontSize: 12 }}>Chat</Text>
+          </View>
+        ),
+      }}
+    />
+
+    <Tab.Screen
+      name='Profile'
+      component={ProfileScreen}
+      options={{
+        tabBarLabel: () => null,
+        tabBarIcon: ({ focused }) => (
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <FontAwesome5 name='user-alt' size={21} color={focused ? '#344E41' : '#9DB2CE'} />
+            <Text style={{ color: focused ? '#344E41' : '#9DB2CE', fontSize: 12 }}>Profile</Text>
+          </View>
+        ),
+      }}
+    />
+
     <Tab.Screen
       name='PlantProfileScreen'
       component={PlantProfileScreen}
@@ -118,45 +143,11 @@ export default BottomTabs;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   navbarContainer: {
     flex: 1,
     paddingTop: 23,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  btn: {
-    width: 42,
-    height: 42,
-    borderRadius: 30,
-    borderWidth: PixelRatio.roundToNearestPixel(1),
-    borderColor: '#3FA96A',
-    backgroundColor: '#ccedcf',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  circle: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    borderStyle: 'solid',
-    borderWidth: 2,
-    borderColor: '#ccedcf',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ccedcf',
-    borderRadius: 25,
-  },
-  text: {
-    fontSize: 13,
-    textAlign: 'center',
-    color: '#3FA96A',
-    paddingTop: 5,
-    fontFamily: 'antipasto',
-    paddingBottom: 8,
   },
 });
