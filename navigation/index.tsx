4f404baf255a64ceb1244f6d55362ddee31e8ec3
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2023-11-19 22:09:57 +0100

    add deep linking

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,23 +1,33 @@
-import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeStackNavigator from './HomeStack';
+import React, { useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import HomeStackNavigator from './HomeStack'
 
-import * as NavigationBar from 'expo-navigation-bar';
-import { useAuth, useSession } from '@clerk/clerk-expo';
+import * as NavigationBar from 'expo-navigation-bar'
+import { useAuth, useSession } from '@clerk/clerk-expo'
+import * as Linking from 'expo-linking'
+
+const prefix = Linking.createURL('/')
 
 const RootNavigator = () => {
   // const { getToken } = useAuth();
-
+  const linking = {
+    prefixes: [prefix],
+    config: {
+      screens: {
+        ChatScreen: 'inbox/',
+      },
+    },
+  }
   // const retrieveToken = async () => {
   //   const userToken = await getToken();
   //   console.log('userTOkrn', userToken);
   // };
   // retrieveToken();
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <HomeStackNavigator />
     </NavigationContainer>
-  );
-};
+  )
+}
 
-export default RootNavigator;
+export default RootNavigator
